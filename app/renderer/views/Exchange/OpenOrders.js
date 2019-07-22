6eb1cea2c8ccec528d9dba0606232479a3ddd331
growlot <growlot.71@gmail.com>
2019-07-23 02:47:55 +1000

    update cancel order logic

--- app/renderer/views/Exchange/OpenOrders.js
+++ app/renderer/views/Exchange/OpenOrders.js
@@ -106,10 +106,13 @@ class OpenOrders extends React.Component {
 									<span>Transaction</span>
 								</div>
 								<div className="item-cancel">
+								 { 
+									(item.status === 'pending' || !tradesContainer.state.isSwapCancelling[item.uuid]) &&
 									<Fail onClick={event => {
 										event.stopPropagation();
 										this.cancelSwap(item.uuid);
 									}}/>
+								}
 								</div>
 							</div>
 						)
